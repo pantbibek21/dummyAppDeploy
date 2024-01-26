cecc7a52ded15eda694d021397472b76554a8c0e
Bibek Pant <pantbibek21@gmail.com>
2024-01-26 07:33:01 +0545

    updated App.jsx with edit

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,7 @@ function App() {
 				</a>
 			</div>
 			<h1>Bibek here (Aatish dai is don)</h1>
-			<h2>
-				Learning CICD (continuous integration continuous deployment)
-			</h2>
+			<h2>Learning CICD , time to test</h2>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
